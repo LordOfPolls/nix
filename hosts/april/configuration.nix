b83ec699ea69ed112443e307d2dd11b3f68de01e
LordOfPolls <dev@lordofpolls.com>
2025-01-17 20:28:08 +0000

    51 current  2025-01-17 20:28:05  24.11.20250115.e24b4c0     6.6.71                          *.

--- hosts/april/configuration.nix
+++ hosts/april/configuration.nix
@@ -13,4 +13,11 @@
   services.xserver.videoDrivers = ["nvidia"];
   hardware.xone.enable = true;
   hardware.xpadneo.enable = true;
+
+  boot = {
+    extraModulePackages = with config.boot.kernelPackages; [xpadneo];
+    extraModprobeConfig = ''
+      options bluetooth disable_ertm=Y
+    '';
+  };
 }
